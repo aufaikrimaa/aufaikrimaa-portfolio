8a58fb1d53fb80b1095048975bc97793800b4007
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-08-07 14:23:28 +0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,20 +176,62 @@ contactsData.forEach((data) => {
   contactsContainer.innerHTML += contactsHTML;
 });
 
+// socmed data
+const socmedData = [
+  {
+    ionic: "mail-outline",
+    link: "mailto:aufaikrimah@gmail.com",
+  },
+  {
+    ionic: "logo-whatsapp",
+
+    link: "https://wa.me/6289506591035",
+  },
+  {
+    ionic: "logo-linkedin",
+    link: "https://www.linkedin.com/in/aufaikrimah/",
+  },
+  {
+    ionic: "logo-instagram",
+    link: "https://www.instagram.com/aufaikrimaa/",
+  },
+];
+
+const socmedContainer = document.querySelector(".socmed-list");
+
+socmedData.forEach((data) => {
+  const socmedHTML = `<a
+      href="${data.link}"
+      target="_blank"
+      ><div
+                  class="icon-box"
+                  style="
+                    height: 60px;
+                    width: 60px;
+                    font-size: 28px;
+                    margin-right: 28px;
+                    cursor: pointer;
+                  "
+                >
+                
+                  <ion-icon name="${data.ionic}"></ion-icon>
+                </div> </a
+    >`;
+
+  socmedContainer.innerHTML += socmedHTML;
+});
+
 // tech stack data
 const techData = [
   "./assets/images/html.svg",
   "./assets/images/css.svg",
   "./assets/images/javascript.svg",
-  "./assets/images/node-js.svg",
+  "./assets/images/typescript.svg",
   "./assets/images/reactjs.svg",
   "./assets/images/redux.svg",
   "./assets/images/vite.svg",
-  "./assets/images/expressjs.svg",
   "./assets/images/tailwind.svg",
   "./assets/images/bootstrap.svg",
-  "./assets/images/mysql.svg",
-  "./assets/images/mongodb.svg",
   "./assets/images/GSAP.png",
 ];
 
@@ -211,189 +253,189 @@ techData.forEach((data) => {
 
 // course data
 
-const courseData = [
-  {
-    image: "./assets/images/logo-pi.svg",
-    track: "Perempuan Inovasi",
-    date: "October, 2023 - present",
-    name: "Full Stack Web Development",
-    content:
-      "Front-end : ReactJs, Tailwind, Bootstrap, Redux. Back-end: ExpressJS, MySQL, MongoDB",
-    link: "https://drive.google.com/file/d/1Z_yzOpTfeV7IA5hoC3XCx4nFxkpfFvQV/view",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/skilvul.svg",
-    track: "Skilvul",
-    date: "June - September, 2023",
-    name: "Web Development",
-    content: "Learning web development with HTML, CSS, and Vanilla Javascript",
-    link: "https://skilvul.com/paths/web-development-pemula-challenge-based/student/clliuhbqq1ivq01qk3t6ccvi1",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/myskill.svg",
-    track: "MySkill",
-    date: "September, 2023",
-    name: "CSS language",
-    content: "",
-    link: "https://drive.google.com/file/d/1Wb9P-oJU865Q87xbKDUUO_mgSiZyi4Pn/view?usp=drive_link",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/logo-bnsp.svg",
-    track: "BNSP",
-    date: "August 02, 2023",
-    name: "Software Development",
-    content: "Certificate of Competence in the area of Software Development",
-    link: "https://drive.google.com/file/d/1PHlPkjjhlYmRcfBzlcRkCyglpLw3XPg7/view?usp=drive_link",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/myskill.svg",
-    track: "MySkill",
-    date: "May, 2023",
-    name: "HTML language",
-    content: "",
-    link: "https://drive.google.com/file/d/13O7evdLokkKLdPpWny6wbbBnZ777l5qP/view?usp=drive_link",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/code.svg",
-    track: "Self learning",
-    date: "January - June, 2023",
-    name: "Framework Laravel & Library ReactJS",
-    content:
-      "I built the TourGo App using the Laravel Framework and the ReactJS Library, learning them on my own through the internet",
-    link: "",
-    credentials: "",
-  },
-  {
-    image: "./assets/images/logo-bsi.svg",
-    track: "UBSI University",
-    date: "Sept - Dec, 2022",
-    name: "Mobile Programming",
-    content: "Learning mobile app development with flutter",
-    link: "",
-    credentials: "",
-  },
-  {
-    image: "./assets/images/logo-bsi.svg",
-    track: "UBSI University",
-    date: "Sept, 2021 - June, 2022",
-    name: "Web Programming",
-    content: "Learning web development with CodeIgniter 3",
-    link: "",
-    credentials: "",
-  },
-  {
-    image: "./assets/images/cisco.svg",
-    track: "Cisco Net Academy",
-    date: "August, 2021",
-    name: "PCAP: Programming Essentials in Python",
-    content: "",
-    link: "https://drive.google.com/file/d/1fmOyFe1XobPqQRxoas6k1FDxfYQ13hwc/view?usp=drive_link",
-    credentials: "see credentials &gt&gt",
-  },
-  {
-    image: "./assets/images/logo-bsi.svg",
-    track: "UBSI University",
-    date: "March - June, 2021",
-    name: "Database System",
-    content: "Learning SQL language for database management",
-    link: "",
-    credentials: "",
-  },
-  {
-    image: "./assets/images/logo-bsi.svg",
-    track: "UBSI University",
-    date: "August, 2020 - Jan 2021",
-    name: "Fundamentals of Python Programming",
-    content: "Learning to build programs with the Python language",
-    link: "",
-    credentials: "",
-  },
-];
+// const courseData = [
+//   {
+//     image: "./assets/images/logo-pi.svg",
+//     track: "Perempuan Inovasi",
+//     date: "October, 2023 - present",
+//     name: "Full Stack Web Development",
+//     content:
+//       "Front-end : ReactJs, Tailwind, Bootstrap, Redux. Back-end: ExpressJS, MySQL, MongoDB",
+//     link: "https://drive.google.com/file/d/1Z_yzOpTfeV7IA5hoC3XCx4nFxkpfFvQV/view",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/skilvul.svg",
+//     track: "Skilvul",
+//     date: "June - September, 2023",
+//     name: "Web Development",
+//     content: "Learning web development with HTML, CSS, and Vanilla Javascript",
+//     link: "https://skilvul.com/paths/web-development-pemula-challenge-based/student/clliuhbqq1ivq01qk3t6ccvi1",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/myskill.svg",
+//     track: "MySkill",
+//     date: "September, 2023",
+//     name: "CSS language",
+//     content: "",
+//     link: "https://drive.google.com/file/d/1Wb9P-oJU865Q87xbKDUUO_mgSiZyi4Pn/view?usp=drive_link",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/logo-bnsp.svg",
+//     track: "BNSP",
+//     date: "August 02, 2023",
+//     name: "Software Development",
+//     content: "Certificate of Competence in the area of Software Development",
+//     link: "https://drive.google.com/file/d/1PHlPkjjhlYmRcfBzlcRkCyglpLw3XPg7/view?usp=drive_link",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/myskill.svg",
+//     track: "MySkill",
+//     date: "May, 2023",
+//     name: "HTML language",
+//     content: "",
+//     link: "https://drive.google.com/file/d/13O7evdLokkKLdPpWny6wbbBnZ777l5qP/view?usp=drive_link",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/code.svg",
+//     track: "Self learning",
+//     date: "January - June, 2023",
+//     name: "Framework Laravel & Library ReactJS",
+//     content:
+//       "I built the TourGo App using the Laravel Framework and the ReactJS Library, learning them on my own through the internet",
+//     link: "",
+//     credentials: "",
+//   },
+//   {
+//     image: "./assets/images/logo-bsi.svg",
+//     track: "UBSI University",
+//     date: "Sept - Dec, 2022",
+//     name: "Mobile Programming",
+//     content: "Learning mobile app development with flutter",
+//     link: "",
+//     credentials: "",
+//   },
+//   {
+//     image: "./assets/images/logo-bsi.svg",
+//     track: "UBSI University",
+//     date: "Sept, 2021 - June, 2022",
+//     name: "Web Programming",
+//     content: "Learning web development with CodeIgniter 3",
+//     link: "",
+//     credentials: "",
+//   },
+//   {
+//     image: "./assets/images/cisco.svg",
+//     track: "Cisco Net Academy",
+//     date: "August, 2021",
+//     name: "PCAP: Programming Essentials in Python",
+//     content: "",
+//     link: "https://drive.google.com/file/d/1fmOyFe1XobPqQRxoas6k1FDxfYQ13hwc/view?usp=drive_link",
+//     credentials: "see credentials &gt&gt",
+//   },
+//   {
+//     image: "./assets/images/logo-bsi.svg",
+//     track: "UBSI University",
+//     date: "March - June, 2021",
+//     name: "Database System",
+//     content: "Learning SQL language for database management",
+//     link: "",
+//     credentials: "",
+//   },
+//   {
+//     image: "./assets/images/logo-bsi.svg",
+//     track: "UBSI University",
+//     date: "August, 2020 - Jan 2021",
+//     name: "Fundamentals of Python Programming",
+//     content: "Learning to build programs with the Python language",
+//     link: "",
+//     credentials: "",
+//   },
+// ];
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("#data-testimonials-item");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
+// // testimonials variables
+// const testimonialsItem = document.querySelectorAll("#data-testimonials-item");
+// const modalContainer = document.querySelector("[data-modal-container]");
+// const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+// const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-const modalDate = document.querySelector("[data-modal-date]");
-const modalCourse = document.querySelector("[data-modal-course]");
-const modalLink = document.querySelector("[data-modal-link]");
+// // modal variable
+// const modalImg = document.querySelector("[data-modal-img]");
+// const modalTitle = document.querySelector("[data-modal-title]");
+// const modalText = document.querySelector("[data-modal-text]");
+// const modalDate = document.querySelector("[data-modal-date]");
+// const modalCourse = document.querySelector("[data-modal-course]");
+// const modalLink = document.querySelector("[data-modal-link]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-};
+// const testimonialsModalFunc = function () {
+//   modalContainer.classList.toggle("active");
+//   overlay.classList.toggle("active");
+// };
 
-const courseContainer = document.querySelector(".testimonials-list");
+// const courseContainer = document.querySelector(".testimonials-list");
 
-function addEventListenersToTestimonialsItems() {
-  const testimonialsItems = document.querySelectorAll(
-    "#data-testimonials-item"
-  );
-  testimonialsItems.forEach(function (testimonialsItem) {
-    testimonialsItem.addEventListener("click", function () {
-      modalImg.src = this.querySelector("#data-testimonials-avatar").src;
-      modalImg.alt = this.querySelector("#data-testimonials-avatar").alt;
-      modalTitle.innerHTML = this.querySelector(
-        "#data-testimonials-title"
-      ).innerHTML;
-      modalText.innerHTML = this.querySelector(
-        "#data-testimonials-text"
-      ).innerHTML;
-      modalDate.innerHTML = this.querySelector(
-        "#data-testimonials-date"
-      ).innerHTML;
-      modalCourse.innerHTML = this.querySelector(
-        "#data-testimonials-course"
-      ).innerHTML;
-      modalLink.href = this.querySelector("#data-testimonials-link").href;
+// function addEventListenersToTestimonialsItems() {
+//   const testimonialsItems = document.querySelectorAll(
+//     "#data-testimonials-item"
+//   );
+//   testimonialsItems.forEach(function (testimonialsItem) {
+//     testimonialsItem.addEventListener("click", function () {
+//       modalImg.src = this.querySelector("#data-testimonials-avatar").src;
+//       modalImg.alt = this.querySelector("#data-testimonials-avatar").alt;
+//       modalTitle.innerHTML = this.querySelector(
+//         "#data-testimonials-title"
+//       ).innerHTML;
+//       modalText.innerHTML = this.querySelector(
+//         "#data-testimonials-text"
+//       ).innerHTML;
+//       modalDate.innerHTML = this.querySelector(
+//         "#data-testimonials-date"
+//       ).innerHTML;
+//       modalCourse.innerHTML = this.querySelector(
+//         "#data-testimonials-course"
+//       ).innerHTML;
+//       modalLink.href = this.querySelector("#data-testimonials-link").href;
 
-      testimonialsModalFunc();
-    });
-  });
-}
+//       testimonialsModalFunc();
+//     });
+//   });
+// }
 
-// Add event listeners to existing testimonials items
-addEventListenersToTestimonialsItems();
+// // Add event listeners to existing testimonials items
+// addEventListenersToTestimonialsItems();
 
-// Function to add testimonials items and event listeners
-function addTestimonialAndEventListener(data) {
-  const courseHTML = `<div class="testimonials-item">
-    <div class="content-card" id="data-testimonials-item">
-      <figure class="testimonials-avatar-box">
-        <img src="${data.image}" alt="${data.track}" id="data-testimonials-avatar" />
-      </figure>
-      <h4 class="h4 testimonials-item-title" id="data-testimonials-title">${data.track}</h4>
-      <h5 class="h5" id="data-testimonials-course">${data.name}</h5>
-      <time id="data-testimonials-date">${data.date}</time>
-      <div class="testimonials-text" id="data-testimonials-text">
-        <p>${data.content}</p>
-      </div>
-      <a href="${data.link}" target="_blank" id="data-testimonials-link" class="course-testimonials-link">${data.credentials}</a>
-    </div>
-  </div>`;
+// // Function to add testimonials items and event listeners
+// function addTestimonialAndEventListener(data) {
+//   const courseHTML = `<div class="testimonials-item">
+//     <div class="content-card" id="data-testimonials-item">
+//       <figure class="testimonials-avatar-box">
+//         <img src="${data.image}" alt="${data.track}" id="data-testimonials-avatar" />
+//       </figure>
+//       <h4 class="h4 testimonials-item-title" id="data-testimonials-title">${data.track}</h4>
+//       <h5 class="h5" id="data-testimonials-course">${data.name}</h5>
+//       <time id="data-testimonials-date">${data.date}</time>
+//       <div class="testimonials-text" id="data-testimonials-text">
+//         <p>${data.content}</p>
+//       </div>
+//       <a href="${data.link}" target="_blank" id="data-testimonials-link" class="course-testimonials-link">${data.credentials}</a>
+//     </div>
+//   </div>`;
 
-  courseContainer.innerHTML += courseHTML;
-  addEventListenersToTestimonialsItems(); // Add event listener to the newly added testimonial
-}
+//   courseContainer.innerHTML += courseHTML;
+//   addEventListenersToTestimonialsItems(); // Add event listener to the newly added testimonial
+// }
 
-// Add testimonials and event listeners
-courseData.forEach(addTestimonialAndEventListener);
+// // Add testimonials and event listeners
+// courseData.forEach(addTestimonialAndEventListener);
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+// // add click event to modal close button
+// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+// overlay.addEventListener("click", testimonialsModalFunc);
 
 // projects data
 
@@ -402,7 +444,7 @@ const projectsData = [
     id: 1,
     title: "LensaKita",
     photo: "./assets/images/lensakita.png",
-    lang: "ReactJs, ExpressJs, Tailwind, GSAP",
+    lang: "ReactJs, Tailwind, GSAP",
     desc: "Web Profile of LensaKita.",
     link: "https://lensakita.vercel.app/",
     github: "https://github.com/aufaikrimaa/lensakita.git",
@@ -420,9 +462,9 @@ const projectsData = [
   },
   {
     id: 3,
-    title: "KJTG Company Profile",
+    title: "KJTG Profile",
     photo: "./assets/images/kjtg.png",
-    lang: "ReactJs, ExpressJS, Tailwind, Shadcn UI",
+    lang: "ReactJs, Tailwind, Shadcn UI",
     desc: "Web Profile of Jeep Tour Galunggung Community.",
     link: "https://komunitasjeeptourgalunggung.netlify.app/",
     github: "https://github.com/aufaikrimaa/kjtg-company-profile.git",
